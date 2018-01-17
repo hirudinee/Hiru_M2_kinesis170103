f79f0439cd650abf9fd8150d0ccb470bca4a03be
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-17 16:07:23 +0530

    kinesis put added

--- Hiru_M2_kinesis170103/Hiru_M2_kinesis170103-lambda.js
+++ Hiru_M2_kinesis170103/Hiru_M2_kinesis170103-lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const kinesis = new AWS.Kinesis();
 
 
 exports.handler = function (event, context, callback) {
@@ -6,4 +7,20 @@ exports.handler = function (event, context, callback) {
 
 
 	callback(null, 'Successfully executed');
+
+
+
+	kinesis.putRecord({
+		Data: 'data',
+		PartitionKey: '0001',
+		StreamName: 'Hiru_Test'
+	}).promise()
+		.then(putRecordData => {
+			// your logic goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
+
+
 }
